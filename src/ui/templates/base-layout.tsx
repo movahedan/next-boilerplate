c94421534a7682b7120de60d11342974977b4f8d
Soheil Movahedan <soheil.movahhedan@gmail.com>
2021-08-29 09:23:20 +0430

    Implement external css with styled-jsx

--- src/ui/templates/base-layout.tsx
+++ src/ui/templates/base-layout.tsx
@@ -1,43 +1,50 @@
 import Link from 'next/link';
+import css from 'styled-jsx/css';
 
 import type { FC } from 'react';
 
 export const BaseLayout: FC = ({ children }) => (
 	<>
+		<style jsx global>
+			{globalCSS}
+		</style>
+
+		<style jsx>{navbarCSS}</style>
 		<nav>
 			<Link href='/'>
 				<a>Go to home</a>
 			</Link>
-			<Link href='/redux-sample'>
-				<a>Go to /redux-sample</a>
+			<Link href='/react-query/client-side'>
+				<a>Go to /react-query</a>
 			</Link>
-			<Link href='/redux-sample/server-side-props'>
-				<a>Go to /redux-sample/server-side-props</a>
+			<Link href='/react-query/static-props'>
+				<a>Go to /react-query/static-props</a>
 			</Link>
 		</nav>
+
 		{children}
-
-		<style jsx global>{`
-			*:not(i) {
-				font-family: Inter sans-serif;
-			}
-
-			#__next {
-				@apply relative flex flex-col min-h-screen m-0 overflow-hidden text-base max-w-screen;
-
-				padding: env(safe-area-inset-top) env(safe-area-inset-right)
-					env(safe-area-inset-bottom) env(safe-area-inset-left);
-			}
-		`}</style>
-
-		<style jsx>{`
-			nav {
-				@apply p-3 text-white bg-black;
-
-				a {
-					@apply p-3 mr-6;
-				}
-			}
-		`}</style>
 	</>
 );
+
+const navbarCSS = css`
+	nav {
+		@apply p-3 text-white bg-black;
+
+		a {
+			@apply p-3 mr-6;
+		}
+	}
+`;
+
+const globalCSS = css.global`
+	*:not(i) {
+		font-family: Inter sans-serif;
+	}
+
+	#__next {
+		@apply relative flex flex-col min-h-screen m-0 overflow-hidden text-base max-w-screen;
+
+		padding: env(safe-area-inset-top) env(safe-area-inset-right)
+			env(safe-area-inset-bottom) env(safe-area-inset-left);
+	}
+`;
