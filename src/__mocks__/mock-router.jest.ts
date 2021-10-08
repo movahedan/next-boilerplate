0b8c193ea31f20401a63b74accde668878812b7a
Soheil Movahedan <soheil.movahhedan@gmail.com>
2021-10-08 20:41:42 +0330

    Create mockUseRouter

---
+++ src/__mocks__/mock-router.jest.ts
@@ -0,0 +1,43 @@
+import * as nextRouter from 'next/router';
+
+type MockUseRouterParams = Partial<nextRouter.NextRouter>;
+type MockUseRouter = (args: MockUseRouterParams) => MockUseRouterParams;
+
+export const mockUseRouter: MockUseRouter = ({
+	route = '',
+	pathname = '',
+	query = {},
+	asPath = '',
+	basePath = '',
+	locale = '',
+	locales = [],
+	defaultLocale = '',
+}) => {
+	const actions = {
+		push: jest.fn(() => Promise.resolve(true)),
+		replace: jest.fn(() => Promise.resolve(true)),
+		reload: jest.fn(() => Promise.resolve(true)),
+		prefetch: jest.fn(() => Promise.resolve()),
+		back: jest.fn(() => Promise.resolve(true)),
+		beforePopState: jest.fn(() => Promise.resolve(true)),
+		events: {
+			on: jest.fn(),
+			off: jest.fn(),
+			emit: jest.fn(),
+		},
+	};
+
+	(nextRouter.useRouter as jest.Mock) = jest.fn(() => ({
+		route,
+		pathname,
+		query,
+		asPath,
+		basePath,
+		locale,
+		locales,
+		defaultLocale,
+		...actions,
+	}));
+
+	return actions;
+};
