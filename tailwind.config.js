7f34fcfb5085092e9b536ba4b5ee5db3441fbc27
Soheil Movahedan <soheil.movahhedan@gmail.com>
2021-10-09 03:23:43 +0330

    Update tailwind config, add some rules

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,34 +4,59 @@
  * @type {import("tailwindcss/tailwind-config").TailwindConfig}
  **/
 module.exports = {
-	purge: {
-		mode: 'all',
-		options: {
-			keyframes: true,
-			fontFace: true,
-		},
-		preserveHtmlElements: false,
-		content: [],
-	},
-	// mode: 'jit',
+	purge: ['./src/{ui,pages}/**/*.{ts,tsx}'],
 	darkMode: false,
 	theme: {
-		screens: {
-			sm: '480px',
-			md: '768px',
-			lg: '1280px',
-		},
 		extend: {
+			width: {
+				'240px': '15rem',
+			},
+			screens: {
+				sm: '480px',
+				md: '768px',
+				lg: '1280px',
+			},
+			lineHeight: {
+				'14px': '0.875rem',
+				'18px': '1.125rem',
+				'24px': '1.5rem',
+				'28px': '1.758rem',
+				'42px': '2.636875rem',
+			},
+			fontSize: {
+				'12px': '0.76rem',
+				'14px': '0.875rem',
+				'18px': '1.125rem',
+				'24px': '1.5rem',
+				'36px': '1.625rem',
+			},
+			fontWeight: {
+				normal: 'normal',
+				bold: 'bold',
+			},
+			textColor: {
+				primary: {
+					default: '#FFFFFF',
+					light: '#BDBDBD',
+					'on-dark-bg': '#424242',
+				},
+				secondary: '#4DF8A6',
+			},
+			backgroundColor: {
+				white: '#FFFFFF',
+				dark: '#212121',
+				lightopacity: 'rgba(255, 255, 255, 0.1)',
+			},
 			maxWidth: {
 				screen: '100vw',
+				'640px': '40rem',
 			},
 		},
 	},
 	variants: {
-		extend: {},
+		extend: {
+			margin: ['last', 'responsive'],
+		},
 	},
 	plugins: [],
-	corePlugins: {
-		float: false,
-	},
 };
