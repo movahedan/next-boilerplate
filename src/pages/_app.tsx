d4e3dc981607d58963332000c8b0cd55b4288229
Soheil Movahedan <soheil.movahhedan@gmail.com>
2021-10-09 04:24:35 +0330

    Connect _app.tsx to next-seo and FontHead:
    Also code of reportWebVitals moved for readablity

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-/* eslint-disable @next/next/no-css-tags */
+import { DefaultSeo } from 'next-seo';
 import App from 'next/app';
 import Head from 'next/head';
 import { QueryClient, QueryClientProvider } from 'react-query';
@@ -7,13 +7,14 @@ import { Hydrate } from 'react-query/hydration';
 
 import { AnalyticsProvider } from 'lib/analytics';
 import { BrowserProvider, extractBrowserServerSideData } from 'lib/browser';
+import { defaultNextSeoConfig } from 'lib/constants';
 
-import { BaseLayout, GlobalCSS } from 'ui';
-import 'tailwindcss/tailwind.css';
+import { BaseLayout, FontLink, GlobalCSS } from 'ui';
 
 import Error from './_error';
 
 import type { AppWithLayoutProps, NextWebVitalsMetric } from 'next/app';
+import 'tailwindcss/tailwind.css';
 
 class MyApp extends App<AppWithLayoutProps> {
 	state = {
@@ -69,31 +70,18 @@ class MyApp extends App<AppWithLayoutProps> {
 	}
 }
 
-const IS_WEB_VITALS_ENABLE = process.env.NEXT_PUBLIC_WEB_VITALS === '1';
-export const reportWebVitals = (metric: NextWebVitalsMetric): void => {
-	if (IS_WEB_VITALS_ENABLE) console.log(metric);
-};
-
-const FontHead = () => (
-	<>
-		<link rel='preconnect' href='https://css.gstatic.com/' crossOrigin='true' />
-		<link rel='preload' as='style' href='/css/font.css' />
-		<link rel='stylesheet' href='/css/font.css' />
-	</>
-);
-
 const DefaultHead = () => (
 	<Head>
 		<meta charSet='utf-8' />
-		<meta httpEquiv='X-UA-Compatible' content='IE=edge' />
-		<meta
-			name='viewport'
-			content='width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=5,user-scalable=yes'
-		/>
-		<title>Next.js</title>
 
-		<FontHead />
+		<DefaultSeo {...defaultNextSeoConfig} />
+		<FontLink />
 	</Head>
 );
+
+const IS_WEB_VITALS_ENABLE = process.env.NEXT_PUBLIC_WEB_VITALS === 'true';
+export const reportWebVitals = (metric: NextWebVitalsMetric): void => {
+	if (IS_WEB_VITALS_ENABLE) console.log(metric);
+};
 
 export default MyApp;
