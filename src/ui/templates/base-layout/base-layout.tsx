6cf5e37f0702c937e64107de88beb09b49db8b16
Soheil Movahedan <soheil.movahhedan@gmail.com>
2021-10-09 07:19:07 +0330

    Update base-layout

--- src/ui/templates/base-layout/base-layout.tsx
+++ src/ui/templates/base-layout/base-layout.tsx
@@ -1,60 +1,18 @@
-import Link from 'next/link';
-import css from 'styled-jsx/css';
-
 import type { FC, CSSProperties } from 'react';
 
-export const BaseLayout: FC<{ style?: CSSProperties; className?: string }> = ({
+interface BaseLayoutProps {
+	style?: CSSProperties;
+	className?: string;
+}
+
+export const BaseLayout: FC<BaseLayoutProps> = ({
 	style,
 	className,
 	children,
 }) => (
 	<>
-		<style jsx global>
-			{globalCSS}
-		</style>
-
-		<style jsx>{navbarCSS}</style>
-		<nav>
-			<Link href='/'>
-				<a>Go to home</a>
-			</Link>
-			<Link href='/pets'>
-				<a>See Pets!</a>
-			</Link>
-			<Link href='/react-query/client-side'>
-				<a>Go to /react-query</a>
-			</Link>
-			<Link href='/react-query/static-props'>
-				<a>Go to /react-query/static-props</a>
-			</Link>
-		</nav>
-
 		<main style={style} className={className}>
 			{children}
 		</main>
 	</>
 );
-
-const navbarCSS = css`
-	nav {
-		@apply p-3 text-white bg-black;
-
-		a {
-			@apply p-3 mr-6;
-		}
-	}
-`;
-
-// eslint-disable-next-line import/no-named-as-default-member
-const globalCSS = css.global`
-	*:not(i) {
-		font-family: Inter sans-serif;
-	}
-
-	:global(#__next) {
-		@apply relative flex flex-col min-h-screen m-0 overflow-hidden text-base max-w-screen;
-
-		padding: env(safe-area-inset-top) env(safe-area-inset-right)
-			env(safe-area-inset-bottom) env(safe-area-inset-left);
-	}
-`;
