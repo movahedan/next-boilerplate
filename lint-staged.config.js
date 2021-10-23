9a358a8ea3eee8d7e33d2d17c78f6cf07ff9a1a6
Soheil Movahedan <soheil.movahhedan@gmail.com>
2021-10-23 18:40:18 +0330

    Change lint-staged to run test for changed files only

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,5 @@
 module.exports = {
 	'**/*.ts?(x)': () => 'yarn type-check',
 	'**/*.(ts)?(x)': () => 'yarn lint',
-	'**/*.test.ts?(x)': () => 'yarn test',
+	'**/*.test.ts?(x)': () => 'yarn test -o',
 };
