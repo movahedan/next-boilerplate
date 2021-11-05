4411f0a31f10ab484eac7c1a9b877c58442b374c
Soheil Movahedan <soheil.movahhedan@gmail.com>
2021-11-05 09:11:01 +0330

    Add vanilla emotion

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { css } from '@emotion/css';
 import { NextSeo } from 'next-seo';
 
 import { globalGetServerSideProps } from 'lib/utils';
@@ -12,10 +13,23 @@ interface Props {
 	};
 }
 
+const basicStyles = css`
+	@apply bg-black;
+	color: cornflowerblue;
+	border: 1px solid lightgreen;
+	border-right: none;
+	border-bottom: none;
+	box-shadow: 5px 5px 0 0 lightgreen, 10px 10px 0 0 lightyellow;
+	transition: all 0.1s linear;
+	margin: 3rem 0;
+	padding: 1rem 0.5rem;
+`;
+
 const IndexPage: NextPageWithLayout<Props> = ({ data: { message } }) => (
 	<>
 		<NextSeo canonical={`${websiteBaseUrl}/`} />
 		<h1>{message}</h1>
+		<div className={basicStyles}>Basic styles using emotion</div>
 	</>
 );
 
