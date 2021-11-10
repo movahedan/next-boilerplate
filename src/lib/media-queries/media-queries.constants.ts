85ee95cfaff189c39b9ead127bd7e5aca808c5b6
Soheil Movahedan <soheil.movahhedan@gmail.com>
2021-11-10 16:22:16 +0330

    Fix functionality of media-queries

--- src/lib/media-queries/media-queries.constants.ts
+++ src/lib/media-queries/media-queries.constants.ts
@@ -4,7 +4,10 @@ import type { ScreenNames } from './media-queries.d';
 
 export const mediaQueriesEntries: [ScreenNames, string][] = Object.entries(
 	tailwindConfig.theme.screens
-).map(([key, value]) => [key as ScreenNames, `(min-width: ${value})`]);
+).map(([key, value], index) => [
+	key as ScreenNames,
+	`(min-width: ${index === 0 ? '0' : value})`,
+]);
 
 export const getMatchMediaEntries: () => [
 	ScreenNames,
