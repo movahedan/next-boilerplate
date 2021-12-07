b2c39e2d01669f9a10b44c9986ceb37796b5f6cc
Soheil Movahedan <soheil.movahhedan@gmail.com>
2021-12-07 06:47:06 +0330

    Add some tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 // @ts-check
 
 const toRem = (value) => `${value / 16}rem`;
-const fontList = ['sans-serif', 'Red Hat Mono'];
+const fontList = ['sans-serif', 'Sen'];
+const fontHeroList = ['sans-serif', 'Sen', 'Red Hat Mono'];
 
 /**
  * @type {import("tailwindcss/tailwind-config").TailwindConfig}
@@ -17,11 +18,34 @@ module.exports = {
 		},
 		backgroundColor: {
 			white: '#FFFFFF',
+			'white-opacity-10': 'rgba(255, 255, 255, 0.1)',
 			dark: '#212121',
+			blue: '#3AB3DA',
+			yellow: '#FFC93D',
+			mouse: '#121212',
 		},
-		textColor: {
+		colors: {
+			transparent: 'transparent',
 			white: '#FFFFFF',
 			black: '#000000',
+			yellow: '#FFC93D',
+			blue: '#3AB3DA',
+			mellow: '#888888',
+			gray: '#B5B5B5',
+			primary: {
+				default: '#424242',
+				light: '#BDBDBD',
+			},
+			secondary: '#4DF8A6',
+		},
+		textColor: {
+			transparent: 'transparent',
+			white: '#FFFFFF',
+			black: '#000000',
+			yellow: '#FFC93D',
+			blue: '#3AB3DA',
+			mellow: '#888888',
+			gray: '#B5B5B5',
 			primary: {
 				default: '#424242',
 				light: '#BDBDBD',
@@ -34,6 +58,15 @@ module.exports = {
 		fontWeight: {},
 		fontFamily: {},
 		typography: {
+			'hero-xl': {
+				css: {
+					fontFamily: fontHeroList,
+					fontSize: toRem(64),
+					lineHeight: toRem(100),
+					fontWeight: '500',
+				},
+			},
+
 			'heading-sm': {
 				css: {
 					fontFamily: fontList,
@@ -64,10 +97,28 @@ module.exports = {
 			'heading-xl': {
 				css: {
 					fontFamily: fontList,
-					fontSize: toRem(24),
-					lineHeight: toRem(42),
+					fontSize: toRem(32),
+					lineHeight: toRem(40),
 					fontWeight: 'bold',
 					maxWidth: toRem(640),
+				},
+			},
+			'heading-2xl': {
+				css: {
+					fontFamily: fontList,
+					fontSize: toRem(48),
+					lineHeight: toRem(40),
+					fontWeight: 'bold',
+					maxWidth: toRem(640),
+				},
+			},
+
+			'wow-xl': {
+				css: {
+					fontFamily: fontList,
+					fontSize: toRem(80),
+					lineHeight: toRem(96),
+					fontWeight: '700',
 				},
 			},
 
@@ -102,16 +153,43 @@ module.exports = {
 				css: {
 					fontFamily: fontList,
 					fontSize: toRem(24),
-					lineHeight: toRem(42),
+					lineHeight: toRem(40),
 					fontWeight: 'normal',
-					maxWidth: toRem(640),
+				},
+			},
+			'2xl': {
+				css: {
+					fontFamily: fontList,
+					fontSize: toRem(32),
+					lineHeight: toRem(38),
+					fontWeight: 'normal',
 				},
 			},
 		},
 
 		extend: {
+			borderColor: {
+				mouse: 'rgba(255, 255, 255, 0.3)',
+			},
+			borderWidth: {
+				'3px': toRem(3),
+			},
+			borderRadius: {
+				'6px': toRem(6),
+				'20px': toRem(20),
+			},
+			cursor: {
+				none: 'none',
+			},
 			width: {
+				'72px': toRem(72),
 				'240px': toRem(240),
+				'480px': toRem(480),
+				'960px': toRem(960),
+			},
+			height: {
+				'72px': toRem(72),
+				'70vh': '70vh',
 			},
 			minHeight: {
 				'24px': toRem(24),
@@ -120,6 +198,16 @@ module.exports = {
 				screen: '100vw',
 				'640px': toRem(640),
 			},
+			margin: {
+				'10px': toRem(10),
+				'40px': toRem(40),
+				'80px': toRem(80),
+				'128px': toRem(128),
+			},
+			padding: {
+				'2px': toRem(2),
+			},
+			opacity: {},
 		},
 	},
 	variants: {
