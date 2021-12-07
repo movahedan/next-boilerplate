b16a9440be267352777ff5c79d83ca44ad0af9db
Soheil Movahedan <soheil.movahhedan@gmail.com>
2021-12-07 06:22:09 +0330

    Remove unneeded styles

--- src/ui/utils/global.styles.ts
+++ src/ui/utils/global.styles.ts
@@ -1,7 +1,7 @@
 import { css } from '@emotion/react';
 import xw from 'xwind';
 
-const nextDivStyles = xw`relative overflow-hidden min-h-screen max-w-screen m-0 p-0`;
+const nextDivStyles = xw`relative overflow-hidden min-h-screen max-w-screen`;
 export const globalStyles = css`
 	#__next {
 		${nextDivStyles};
