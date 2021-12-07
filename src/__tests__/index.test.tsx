2fa51e1122c54cba1c16b105ad0c00fe10f84b9a
Soheil Movahedan <soheil.movahhedan@gmail.com>
2021-12-07 10:33:41 +0330

    Create first version of homepage

--- src/__tests__/index.test.tsx
+++ src/__tests__/index.test.tsx
@@ -2,44 +2,17 @@ import { render } from '@testing-library/react';
 import { axe } from 'jest-axe';
 import { create } from 'react-test-renderer';
 
-import Homepage, { getServerSideProps } from 'pages/index';
-
-import type { GetServerSidePropsContext } from 'next';
+import Homepage from 'pages/index';
 
 describe('<Homepage>', () => {
 	it('should match snapshot', async () => {
-		const component = create(
-			<Homepage data={{ message: 'Message from getServerSideProps' }} />
-		).toJSON();
+		const component = create(<Homepage />).toJSON();
 		expect(component).toMatchSnapshot();
 	});
 
 	it('should be accessible', async () => {
-		const { container } = render(
-			<Homepage data={{ message: 'Message from getServerSideProps' }} />
-		);
+		const { container } = render(<Homepage />);
 		const results = await axe(container);
 		expect(results).toHaveNoViolations();
-	});
-
-	it('should have getServerSideProps', async () => {
-		const context = {
-			req: { headers: { 'user-agent': '' } },
-			res: { setHeader: jest.fn() },
-			query: {},
-			resolvedUrl: '/',
-		} as unknown as GetServerSidePropsContext;
-
-		const results = await getServerSideProps(context);
-		expect(results).toEqual({
-			props: {
-				data: {
-					message: 'Message from getServerSideProps',
-				},
-				mediaQueries: {
-					isMobile: false,
-				},
-			},
-		});
 	});
 });
