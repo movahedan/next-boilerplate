c3f3bca75d0a9c6ddf8572a83b2149d0099738d7
Soheil Movahedan <soheil.movahhedan@gmail.com>
2021-12-07 10:33:41 +0330

    Create first version of homepage

---
+++ src/entry/contents/portfolio.tsx
@@ -0,0 +1,80 @@
+export const portfolioContent = {
+	jumbotron: {
+		intro: 'I’m',
+		name: 'Soheil Movahedan',
+		infoList: ['A front-end developer', 'Really into coding'],
+
+		description:
+			'A JavaScript developer with 5 years of experience in Front-End development. Skills include fast learning, creative problem solving and high perseverance. Able to communicate effectively in a team and very keen to improve my management skills.',
+
+		actionButtons: {
+			knowMe: 'Know Me More',
+			downloadCv: 'Just Download CV',
+		},
+	},
+	findMe: {
+		title: 'Find Me at',
+		list: [
+			{
+				type: 'email',
+				label: 'soheil.movahhedan@gmail.com',
+				icon: 'M44 12C44 9.8 42.2 8 40 8H8C5.8 8 4 9.8 4 12V36C4 38.2 5.8 40 8 40H40C42.2 40 44 38.2 44 36V12ZM40 12L24 22L8 12H40ZM40 36H8V16L24 26L40 16V36Z',
+			},
+			{
+				type: 'linkedin',
+				label: 'https://linkedin.com/in/soheil-movahedan',
+				icon: 'M24 4C21.3736 4 18.7728 4.51732 16.3463 5.52241C13.9198 6.5275 11.715 8.00069 9.85786 9.85786C6.10714 13.6086 4 18.6957 4 24C4 32.84 9.74 40.34 17.68 43C18.68 43.16 19 42.54 19 42C19 41.54 19 40.28 19 38.62C13.46 39.82 12.28 35.94 12.28 35.94C11.36 33.62 10.06 33 10.06 33C8.24 31.76 10.2 31.8 10.2 31.8C12.2 31.94 13.26 33.86 13.26 33.86C15 36.9 17.94 36 19.08 35.52C19.26 34.22 19.78 33.34 20.34 32.84C15.9 32.34 11.24 30.62 11.24 23C11.24 20.78 12 19 13.3 17.58C13.1 17.08 12.4 15 13.5 12.3C13.5 12.3 15.18 11.76 19 14.34C20.58 13.9 22.3 13.68 24 13.68C25.7 13.68 27.42 13.9 29 14.34C32.82 11.76 34.5 12.3 34.5 12.3C35.6 15 34.9 17.08 34.7 17.58C36 19 36.76 20.78 36.76 23C36.76 30.64 32.08 32.32 27.62 32.82C28.34 33.44 29 34.66 29 36.52C29 39.2 29 41.36 29 42C29 42.54 29.32 43.18 30.34 43C38.28 40.32 44 32.84 44 24C44 21.3736 43.4827 18.7728 42.4776 16.3463C41.4725 13.9198 39.9993 11.715 38.1421 9.85786C36.285 8.00069 34.0802 6.5275 31.6537 5.52241C29.2272 4.51732 26.6264 4 24 4Z',
+			},
+			{
+				type: 'github',
+				label: 'https://github.com/movahedan',
+				icon: 'M38 6C39.0609 6 40.0783 6.42143 40.8284 7.17157C41.5786 7.92172 42 8.93913 42 10V38C42 39.0609 41.5786 40.0783 40.8284 40.8284C40.0783 41.5786 39.0609 42 38 42H10C8.93913 42 7.92172 41.5786 7.17157 40.8284C6.42143 40.0783 6 39.0609 6 38V10C6 8.93913 6.42143 7.92172 7.17157 7.17157C7.92172 6.42143 8.93913 6 10 6H38ZM37 37V26.4C37 24.6708 36.3131 23.0124 35.0903 21.7897C33.8676 20.5669 32.2092 19.88 30.48 19.88C28.78 19.88 26.8 20.92 25.84 22.48V20.26H20.26V37H25.84V27.14C25.84 25.6 27.08 24.34 28.62 24.34C29.3626 24.34 30.0748 24.635 30.5999 25.1601C31.125 25.6852 31.42 26.3974 31.42 27.14V37H37ZM13.76 17.12C14.6511 17.12 15.5058 16.766 16.1359 16.1359C16.766 15.5058 17.12 14.6511 17.12 13.76C17.12 11.9 15.62 10.38 13.76 10.38C12.8636 10.38 12.0039 10.7361 11.37 11.37C10.7361 12.0039 10.38 12.8636 10.38 13.76C10.38 15.62 11.9 17.12 13.76 17.12ZM16.54 37V20.26H11V37H16.54Z',
+			},
+		],
+	},
+	stack: {
+		title: 'I’ve Worked on',
+		list: [],
+	},
+	worked: {
+		title: 'I’ve Worked in',
+		list: [
+			{
+				title: '👨🏻‍ Senior Front-end Developer',
+				type: 'Full-time',
+				company: 'Jaytoon',
+				duration: '📆 September 2020  –present 5 months',
+				location: '🏢 Sydney, Australia (Remote, International product)',
+				description: `As a team member, I'm working here on a task/project basis. job. Working on various projects in agile.`,
+			},
+			{
+				title: '👨🏻‍ Senior Front-end Developer',
+				company: 'Techlinked',
+				type: 'Full-time',
+				duration: '📆 October 2019  –present 17 months',
+				location: '🏢 California, USA (Remote, International product)',
+				description: `A platform for industrial equipment, their experts and their service-providing companies that includes a series of social-media features like feed, follow, etc. The company is based in Silicon valley.`,
+			},
+			{
+				title: '👨🏻‍ Senior Front-end Developer',
+				company: 'Techlinked',
+				type: 'Full-time',
+				duration: '📆 October 2019  –present 17 months',
+				location: '🏢 California, USA (Remote, International product)',
+				description: `A platform for industrial equipment, their experts and their service-providing companies that includes a series of social-media features like feed, follow, etc. The company is based in Silicon valley.`,
+			},
+			{
+				title: '👨🏻‍ Senior Front-end Developer',
+				company: 'Techlinked',
+				type: 'Full-time',
+				duration: '📆 October 2019  –present 17 months',
+				location: '🏢 California, USA (Remote, International product)',
+				description: `A platform for industrial equipment, their experts and their service-providing companies that includes a series of social-media features like feed, follow, etc. The company is based in Silicon valley.`,
+			},
+		],
+	},
+	tips: {
+		title: 'Do you agree',
+		list: [],
+	},
+};
