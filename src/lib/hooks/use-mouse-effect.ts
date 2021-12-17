cfed220a9659251e8eb8179a04e44b717b8582eb
Soheil Movahedan <soheil.movahhedan@gmail.com>
2021-12-17 13:13:10 +0330

    Fix mouse event listener

--- src/lib/hooks/use-mouse-effect.ts
+++ src/lib/hooks/use-mouse-effect.ts
@@ -1,5 +1,5 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { useRef, useEffect, useCallback } from 'react';
+import { useRef, useEffect, useState } from 'react';
 
 import type { DependencyList } from 'react';
 
@@ -7,28 +7,47 @@ type MouseEffect = (event: MouseEvent) => void;
 export const useMouseEffect = (callback: MouseEffect, deps: DependencyList) => {
 	const isLock = useRef(false);
 	const animationFrame = useRef(0);
-
-	const onMouseMove = useCallback<MouseEffect>((event) => {
-		if (!isLock.current) {
-			isLock.current = true;
-			cancelAnimationFrame(animationFrame.current);
-
-			animationFrame.current = requestAnimationFrame(() => {
-				callback(event);
-			});
-		} else {
-			setTimeout(() => {
-				isLock.current = false;
-			}, 10);
-		}
-	}, deps);
+	const [documentIsVisible, setDocumentIsVisible] = useState(true);
 
 	useEffect(() => {
+		const onVisibilityChange = () => {
+			if (document.visibilityState === 'visible') {
+				setDocumentIsVisible(true);
+			} else {
+				setDocumentIsVisible(false);
+			}
+		};
+
+		document.addEventListener('visibilitychange', onVisibilityChange);
+
+		return () => {
+			document.removeEventListener('visibilitychange', onVisibilityChange);
+		};
+	}, []);
+
+	useEffect(() => {
+		const onMouseMove = (event: MouseEvent) => {
+			if (documentIsVisible) {
+				if (!isLock.current) {
+					isLock.current = true;
+					cancelAnimationFrame(animationFrame.current);
+
+					animationFrame.current = requestAnimationFrame(() => {
+						callback(event);
+					});
+				} else {
+					setTimeout(() => {
+						isLock.current = false;
+					}, 10);
+				}
+			}
+		};
+
 		document.addEventListener('mousemove', onMouseMove);
 
 		return () => {
 			cancelAnimationFrame(animationFrame.current);
 			document.removeEventListener('mousemove', onMouseMove);
 		};
-	}, [onMouseMove]);
+	}, [documentIsVisible, ...deps]);
 };
